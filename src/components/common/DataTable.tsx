e0ec3f969f2128d7478b2f5d83e3db9708fbb4e6
“canhvanlam” <canhvanlam@salescore.me>
2025-04-20 23:01:55 +0700

    fix table

--- src/components/common/DataTable.tsx
+++ src/components/common/DataTable.tsx
@@ -38,7 +38,7 @@ const DataTable = <T extends Record<string, unknown>>({
       tableLayout="fixed"
       rowKey="id"
       scroll={{
-        y: !isMobile ? `calc(61vh - ${heightTable}px)` : "100%",
+        y: !isMobile ? `calc(66.8vh - ${heightTable}px)` : "100%",
         x: "max-content",
       }}
       locale={{
