9f02e9fc06789d873cf7202167c7732cbe56835b
“canhvanlam” <canhvanlam@gmail.com>
2025-10-25 15:00:14 +0700

    fix

--- src/pages/loyalty/program/EarningGeneral.tsx
+++ src/pages/loyalty/program/EarningGeneral.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Card, Row } from "antd";
 import { useMemo, useRef, useState } from "react";
 import useElementHeight from "../../../utils/useElementHeight";
