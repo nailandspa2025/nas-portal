8ccd7e8901fc7426da7708f606919b808dd538cf
canhvanlam <canhvanlam@gmail.com>
2026-07-17 13:58:57 +0700

    Update VoucherPreview.tsx

--- src/pages/loyalty/voucher/VoucherPreview.tsx
+++ src/pages/loyalty/voucher/VoucherPreview.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Card, Divider, Flex, Typography } from "antd";
+import { Divider, Flex, Typography } from "antd";
 import { DollarCircleOutlined, QrcodeOutlined } from "@ant-design/icons";
 import { useTranslation } from "react-i18next";
 
